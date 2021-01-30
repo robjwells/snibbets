71f2fd1ccfd0bd80b2309a8f04bb7a1f919df547
Rob Wells <rob@robjwells.com>
2021-01-30 18:47:30 +0000

    Return result of recursive menu call
    Prevents the user having to enter multiple correct numbers to return to the main flow.

--- Snibbets.lbaction/Contents/Scripts/snibbets.rb
+++ Snibbets.lbaction/Contents/Scripts/snibbets.rb
@@ -111,7 +111,7 @@ def menu(res,title="Select one")
         break
       else
         $stderr.puts "Out of range"
-        menu(res,title)
+        return menu(res,title)
       end
     end
   rescue Interrupt => e
